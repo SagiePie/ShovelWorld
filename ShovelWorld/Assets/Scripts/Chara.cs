1cd5cc0e4de75f8f773b4956f6b695ca5dbd4c2a
Julia Gallagher <dottiespottie12321@gmail.com>
2018-12-01 14:39:29 -0500

    added player movement

---
+++ ShovelWorld/Assets/Scripts/Chara.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chara : MonoBehaviour {
+
+    public GameObject boundary;
+
+    private void FixedUpdate()
+    {
+        //character movement with wasd
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+
+        transform.Translate(x, y, 0);
+    }
+}
---
+++ ShovelWorld/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
