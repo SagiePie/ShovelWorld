d8c1e39238e03a0b4e99c74bdc0f6673d4eff68f
Julia Gallagher <dottiespottie12321@gmail.com>
2018-12-09 13:04:28 -0500

    Menu
    Changed Menu
    Updated ReadMe
    Edited Red Scene

---
+++ README.txt
@@ -0,0 +1,31 @@
+*****************************************************
+            SHOVEL THE WORLD README
+
+*****************************************************
+-------- STORY OVERVIEW ---------
+    Chara wakes in a world of darkness void of any color. Finding a magic shovel they're off to restore color to the world.
+
+-------- CONTROLS ---------
+    For movement use [w] [a] [s] [d] or [up] [left] [right] [down] keys
+    Go to pause menu with [esc]
+    Sprint with [shift]
+    Action with [space]
+
+-------- NOTES ---------
+Menu has chapter select of each color (all unlocked in advanced)
+    Must play from start to see intro
+    Must finish final level for conclusion
+Tilemap was supposed to be randomly generated, but Unity failed to comply complete random generation
+    Random generated loot under each star point 
+
+    
+
+-------- RESOURCES ---------
+Random Tile Generation
+    https://www.youtube.com/watch?v=xNqqfABXTNQ
+    https://blogs.unity3d.com/2018/05/29/procedural-patterns-you-can-use-with-tilemaps-part-i/
+
+Lighting
+    https://unionassets.com/blog/working-with-lighting-in-unity-theory-and-practice-462
+    
+Sound
---
+++ ShovelWorld/Assets/Scripts/UnusedScripts/TileAutomata.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+public class TileAutomata : MonoBehaviour {
+    [Header("Tilemap Vars")]
+    [Range(0, 100)]
+    public int initChance;
+
+    [Range(1, 8)]
+    public int birthLimit;
+
+    [Range(1, 8)]
+    public int deathLimit;
+
+    [Range(1, 10)]
+    public int numR;
+    public int count = 0;
+
+    private int[,] terrainMap;
+    public Vector3Int tmpSize;
+    public Tilemap topMap;
+    public Tilemap bottomMap;
+    //public TerrainTile topTile;
+    //public AnimatedTile bottomTile;
+
+    int width, height;
+
+    public void DoSimulation(int num)
+    {
+        ClearMap(false);
+        width = tmpSize.x;
+        height = tmpSize.y;
+
+        if(terrainMap == null)
+        {
+            terrainMap = new int[width, height];
+            InitPos();
+        }
+
+        for (int i = 0; i < num; i++)
+            terrainMap = GenTilePos(terrainMap);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                //if (terrainMap[x, y] == 1)
+                //    topMap.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), topTile);
+                //bottomTile.SetTile(new Vector3Int(-x + width / 2, -y + height / 2, 0), bottomTile);
+            }
+        }
+    }
+
+    public void InitPos()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                terrainMap[x, y] = Random.Range(1, 101) < initChance ? 1 : 0;
+            }
+        }
+    }
+
+    public int[,] GenTilePos(int[,] oldMap)
+    {
+        int[,] newMap = new int[width, height];
+        int neighbor;
+        BoundsInt myB = new BoundsInt(-1, -1, 0, 3, 3, 1);
+
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                neighbor = 0;
+                foreach(var b in myB.allPositionsWithin)
+                {
+                    if (b.x == 0 && b.y == 0)
+                        continue;
+                    if (x + b.x >= 0 && x + b.x < width && y + b.y >= 0 && y + b.y < height)
+                        neighbor += oldMap[x + b.x, y + b.y];
+                    else
+                        neighbor++;
+                }
+                if(oldMap[x, 1] == 1)
+                {
+                    if (neighbor < deathLimit)
+                        newMap[x, y] = 0;
+                    else
+                        newMap[x, y] = 1;
+                }
+                if(oldMap[x, y] == 0)
+                {
+                    if (neighbor > birthLimit)
+                        newMap[x, y] = 1;
+                    else
+                        newMap[x, y] = 0;
+                }
+            }
+        }
+        return newMap;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            DoSimulation(numR);
+        if (Input.GetMouseButtonDown(1))
+            ClearMap(true);
+        if(Input.GetMouseButtonDown(2))
+        {
+            SaveAssetMap();
+            count++;
+        }
+    }
+
+    public void SaveAssetMap()
+    {
+        string saveName = "tmapXY_" + count;
+        var mapFind = GameObject.Find("Grid");
+
+        if(mapFind)
+        {
+            var savePath = "Assets/Maps/" + saveName + ".prefab";
+            if (PrefabUtility.CreatePrefab(savePath, mapFind))
+                EditorUtility.DisplayDialog("Tilemap saved", "Your Tilemap was saved under: " + savePath, "continue");
+            else
+                EditorUtility.DisplayDialog("Tilemap NOT saved", "An ERROR occured while trying to save Tilemap under: " + savePath, "continue");
+        }
+    }
+
+    public void ClearMap(bool complete)
+    {
+        topMap.ClearAllTiles();
+        bottomMap.ClearAllTiles();
+        if (complete)
+            terrainMap = null;
+    }
+}
